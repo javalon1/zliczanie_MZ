15c233d980c2e352f608d0c0c1fd80480bf07aed
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-12 15:00:55 +0100

    nowy plik

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,23 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App
-    {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int[] a = {4, 1, 6, 1, 1, 4, 3, 3, 1};
+        int n = a.length;
+        int wynik = 0;
+        int x = 1;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x) {
+                wynik = wynik + 1;
+            }
+        }
+        System.out.println(x + "wystepuje: " + wynik + " razy");
+
     }
 }
